7c2f1b1b17332853aafcad782f27c4be91e9e4a2
DanielRizo <danielrizo@wposs.com>
2023-03-13 15:10:54 -0500

    ajustes insert

--- migracion.py
+++ migracion.py
@@ -1,62 +1,91 @@
 from conectionCassandra import conectionCassandra
 from conectionSQLServer import connLocalSQLServer
 
+def migrationData(select, insert):
 
-def migrationData():
+    try:
+        # activar sesion conexion cassandra
+        sesionCassandra = conectionCassandra().connect()
+        ejecutarConsulta = sesionCassandra.execute(select)
 
-    #activar sesion conexion cassandra
-    sesionCassandra = conectionCassandra().connect()
+        # crear value del insert a partir del insert parametro
+        cantidadColumnas = len(ejecutarConsulta.column_types)
+        valueInsert = ""
 
-    consulta = "select id, terminal, transaction_id, estado, forma_pago, authorization_code, channel, date_transaction, debit_amount, " \
-               "debit_coin, debit_currency, exchange_rate, exchange_rate_currency, name_account, period, sequential, service_account, " \
-               "service_detail, service_name, time_transaction, total_amount, total_amount_currency, id_comercio, id_sucursal, id_terminal, " \
-               "nombre_comercio, direccion_sucursal, nombre_encargado, documento_encargado, telefono, nit, id_tipo_transaccion, tipo_transaccion, " \
-               "create_by, created_date, updated_by, updated_date, cliente_id from registrar_pago.registrar_pago;"
+        for i in range(0, cantidadColumnas):
+            iEnString = str(i)
+            # a.append("'{row[" + iEnString + "]}',)")
+            if 'Int' in str(ejecutarConsulta.column_types[i]):
+                valueInsert = valueInsert + "{row[" + iEnString + "]},"
+                continue
 
-    ejecutarConsulta = sesionCassandra.execute(consulta)
+            if i == cantidadColumnas - 1:
+                valueInsert = valueInsert + "'{row[" + iEnString + "]}'"
+                break
+            valueInsert = valueInsert + "'{row[" + iEnString + "]}',"
 
-    dataToList = list(ejecutarConsulta)
-    cont = 0
 
-    #sesion sql server
-    cursor = connLocalSQLServer()
 
-    #activar identity insert en la tabla para realizar los insert
-    actIdentInsert = "SET IDENTITY_INSERT migracion_pasantias.dbo.historico_reportes ON;"
-    cursor.execute(actIdentInsert)
-    connLocalSQLServer().commit()
+        # crear value del insert a partir del insert parametro
 
-    for row in dataToList:
-        a = f"INSERT migracion_pasantias.dbo.historico_reportes (id, terminal, transaction_id, estado, forma_pago, authorization_code, channel, " \
-            f"date_transaction, debit_amount, debit_coin, debit_currency, exchange_rate, exchange_rate_currency, name_account, period, sequential, " \
-            f"service_account, service_detail, service_name, time_transaction, total_amount, total_amount_currency, id_comercio, id_sucursal, " \
-            f"id_terminal, nombre_comercio, direccion_sucursal, nombre_encargado, documento_encargado, telefono, nit, id_tipo_transaccion, " \
-            f"tipo_transaccion, create_by, created_date, updated_by, updated_date, cliente_id) " \
-            f"VALUES ({row[0]}, '{row[1]}', '{row[2]}', '{row[3]}', '{row[4]}', '{row[5]}', '{row[6]}', '{row[7]}', '{row[8]}', '{row[9]}', " \
-            f"'{row[10]}', '{row[11]}', '{row[12]}', '{row[13]}', '{row[14]}', '{row[15]}', '{row[16]}', '{row[17]}', '{row[18]}', '{row[19]}'," \
-            f" '{row[20]}', '{row[21]}', '{row[22]}', '{row[23]}', '{row[24]}', '{row[25]}', '{row[26]}', '{row[27]}', '{row[28]}', '{row[29]}'," \
-            f" '{row[30]}', '{row[31]}', '{row[32]}', '{row[33]}', '{row[34]}', '{row[35]}', '{row[36]}', '{row[37]}')"
+    except Exception as e:
+        print("error en el parametro insert. ", e)
 
-        # activar conexion sql
-        cursor.execute(a)
+    try:
+        #sesion sql server
+        cursor = connLocalSQLServer()
+
+        #activar identity insert en la tabla para realizar los insert
+        actIdentInsert = "SET IDENTITY_INSERT migracion_pasantias.dbo.historico_reportes ON;"
+        cursor.execute(actIdentInsert)
         connLocalSQLServer().commit()
 
-        cont = cont+1
-        print(cont, " insercion exitosa")
+        for row in ejecutarConsulta:
 
-        # if cont == 10:
-        #     break
-
-    #desactivar identity insert en la tabla para realizar los insert
-    desactIdentInsert = "SET IDENTITY_INSERT migracion_pasantias.dbo.historico_reportes OFF;"
-    cursor.execute(desactIdentInsert)
-    connLocalSQLServer().commit()
-
-    connLocalSQLServer().close()
+            insertSQL = f"{insert}" \
+                        f" VALUES({valueInsert})"
+            # activar conexion sql
+            cursor.execute(insertSQL)
+            connLocalSQLServer().commit()
 
 
-try:
-    migrationData()
+            cont = cont+1
+            print(cont, " inserciones exitosas")
+            if cont == 2:
+                break
 
-except Exception as e:
-    print("error en la conexion o insersion. ", e)
+        # desactivar identity insert en la tabla para realizar los insert
+        desactIdentInsert = "SET IDENTITY_INSERT migracion_pasantias.dbo.historico_reportes OFF;"
+        cursor.execute(desactIdentInsert)
+        connLocalSQLServer().commit()
+        connLocalSQLServer().close()
+
+    except Exception as e:
+        print("error en la conexion o insercion. ", e)
+
+
+
+
+
+select1 = "select id, terminal, transaction_id, estado, forma_pago, authorization_code, channel, date_transaction, debit_amount, " \
+       "debit_coin, debit_currency, exchange_rate, exchange_rate_currency, name_account, period, sequential, service_account, " \
+       "service_detail, service_name, time_transaction, total_amount, total_amount_currency, id_comercio, id_sucursal, id_terminal, " \
+       "nombre_comercio, direccion_sucursal, nombre_encargado, documento_encargado, telefono, nit, id_tipo_transaccion, tipo_transaccion, " \
+       "create_by, created_date, updated_by, updated_date, cliente_id from registrar_pago.registrar_pago;"
+
+insert1 = f"INSERT migracion_pasantias.dbo.historico_reportes (id, terminal, transaction_id, estado, forma_pago, authorization_code, channel, " \
+    f"date_transaction, debit_amount, debit_coin, debit_currency, exchange_rate, exchange_rate_currency, name_account, period, sequential, " \
+    f"service_account, service_detail, service_name, time_transaction, total_amount, total_amount_currency, id_comercio, id_sucursal, " \
+    f"id_terminal, nombre_comercio, direccion_sucursal, nombre_encargado, documento_encargado, telefono, nit, id_tipo_transaccion, " \
+    f"tipo_transaccion, create_by, created_date, updated_by, updated_date, cliente_id)"
+
+#     f"VALUES ({row[0]}, '{row[1]}', '{row[2]}', '{row[3]}', '{row[4]}', '{row[5]}', '{row[6]}', '{row[7]}', '{row[8]}', '{row[9]}', " \
+#     f"'{row[10]}', '{row[11]}', '{row[12]}', '{row[13]}', '{row[14]}', '{row[15]}', '{row[16]}', '{row[17]}', '{row[18]}', '{row[19]}'," \
+#     f" '{row[20]}', '{row[21]}', '{row[22]}', '{row[23]}', '{row[24]}', '{row[25]}', '{row[26]}', '{row[27]}', '{row[28]}', '{row[29]}'," \
+#     f" '{row[30]}', '{row[31]}', '{row[32]}', '{row[33]}', '{row[34]}', '{row[35]}', '{row[36]}', '{row[37]}')"
+
+
+migrationData(select1, insert1)
+
+# except Exception as e:
+#     print("error en la conexion o insersion. ", e)
--- test.py
+++ test.py
@@ -1,48 +1,40 @@
-#pruebas unitarias del script
-
+import unittest
+from unittest.mock import patch, MagicMock
 from conectionCassandra import conectionCassandra
 from conectionSQLServer import connLocalSQLServer
+from migracion import migrationData
 
+class TestMigrationData(unittest.TestCase):
 
-def migrationData():
+    def setUp(self):
+        # Crear una base de datos temporal en SQL Server
+        self.conn = connLocalSQLServer()
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('CREATE DATABASE TestDB;')
+        self.cursor.execute('USE TestDB;')
+        self.cursor.execute('CREATE TABLE historico_reportes (id INT IDENTITY(1,1) PRIMARY KEY, terminal VARCHAR(50), transaction_id VARCHAR(50), estado VARCHAR(50));')
+        self.conn.commit()
 
-    #activar sesion conexion cassandra
-    sesionCassandra = conectionCassandra().connect()
+        # Crear una base de datos temporal en Cassandra
+        self.cassandra_session = conectionCassandra().connect()
+        self.cassandra_session.execute('CREATE KEYSPACE TestKS WITH REPLICATION = {\'class\': \'SimpleStrategy\', \'replication_factor\' : 1};')
+        self.cassandra_session.execute('CREATE TABLE TestKS.registrar_pago (id INT PRIMARY KEY, terminal VARCHAR, transaction_id VARCHAR, estado VARCHAR);')
 
-    consulta = "select id, terminal, transaction_id, estado from registrar_pago.registrar_pago;"
+        # Insertar datos de prueba en Cassandra
+        self.cassandra_session.execute('INSERT INTO TestKS.registrar_pago (id, terminal, transaction_id, estado) VALUES (1, \'terminal1\', \'transaction1\', \'estado1\');')
+        self.cassandra_session.execute('INSERT INTO TestKS.registrar_pago (id, terminal, transaction_id, estado) VALUES (2, \'terminal2\', \'transaction2\', \'estado2\');')
+        self.cassandra_session.execute('INSERT INTO TestKS.registrar_pago (id, terminal, transaction_id, estado) VALUES (3, \'terminal3\', \'transaction3\', \'estado3\');')
+        self.cassandra_session.execute('INSERT INTO TestKS.registrar_pago (id, terminal, transaction_id, estado) VALUES (4, \'terminal4\', \'transaction4\', \'estado4\');')
+        self.cassandra_session.execute('INSERT INTO TestKS.registrar_pago (id, terminal, transaction_id, estado) VALUES (5, \'terminal5\', \'transaction5\', \'estado5\');')
 
-    ejecutarConsulta = sesionCassandra.execute(consulta)
+    def tearDown(self):
+        # Eliminar la base de datos temporal en SQL Server
+        self.cursor.execute('DROP DATABASE TestDB;')
+        self.conn.commit()
 
-    dataToList = list(ejecutarConsulta)
-    cont = 0
+        # Eliminar la base de datos temporal en Cassandra
+        self.cassandra_session.execute('DROP KEYSPACE TestKS;')
 
-    #sesion sql server
-    cursor = connLocalSQLServer()
-
-
-    actIdentInsert = "SET IDENTITY_INSERT migracion_pasantias.dbo.historico_reportes ON;"
-    cursor.execute(actIdentInsert)
-    connLocalSQLServer().commit()
-
-    for row in dataToList:
-        a = f"INSERT migracion_pasantias.dbo.historico_reportes (id, terminal, transaction_id, estado) VALUES ({row[0]}, '{row[1]}', '{row[2]}', '{row[3]}')"
-
-        # activar conexion sql
-        cursor.execute(a)
-        connLocalSQLServer().commit()
-
-        cont = cont+1
-        print(cont, " insercion exitosa")
-
-    desactIdentInsert = "SET IDENTITY_INSERT migracion_pasantias.dbo.historico_reportes OFF;"
-    cursor.execute(desactIdentInsert)
-    connLocalSQLServer().commit()
-
-    connLocalSQLServer().close()
-
-
-try:
-    migrationData()
-
-except Exception as e:
-    print("error en la conexion o insersion. ", e)
+    def test_migrationData(self):
+        # Mockear la
+        migrationData()
